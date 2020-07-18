2ee88ec64d72867bd688c0edfc28e5774e6798e8
Daniil <tretyakov.daniil.edcuation@gmail.com>
2020-07-18 21:01:16 +0300

    a bit better practicies

--- schemegen/admin.py
+++ schemegen/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Tree, Choice, Variant, Schema
+from .models import Tree, Choice, Variant, Schema, Template, TextAlias
 
 admin.site.register(Tree)
 admin.site.register(Choice)
 admin.site.register(Variant)
 admin.site.register(Schema)
+admin.site.register(Template)
+admin.site.register(TextAlias)
--- schemegen/models.py
+++ schemegen/models.py
@@ -29,3 +29,19 @@ class Schema(models.Model):
 
     def __str__(self):
         return f'Schema for \'{self.tree.name}\''
+
+class Template(models.Model):
+    name = models.CharField(max_length=200)
+    header = models.TextField()
+    body = models.TextField()
+    footer = models.TextField()
+
+    def __str__(self):
+        return 'Template: ' + self.name
+
+class TextAlias(models.Model):
+    html_id = models.CharField(max_length=50)
+    text = models.TextField()
+
+    def __str__(self):
+        return f'{self.html_id}: {self.text}'
