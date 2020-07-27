7d67ce3a9377db105b22a12ab041e3adb2f34e6a
Daniil <tretyakov.daniil.edcuation@gmail.com>
2020-07-27 14:28:56 +0300

    upd

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -128,3 +127,4 @@ STATICFILES_DIRS = (
 )
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
